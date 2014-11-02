5ecfea38a426ad9d52d93a831fb4c61e3dbc1e4b
Ben Maraney <ben.maraney@klarna.com>
2014-11-02 15:38:07 +0200

    working with 2 images

--- phrase_imaginizer.go
+++ phrase_imaginizer.go
@@ -9,15 +9,14 @@ import (
   "image"
   "bytes"
   "image/png"
-  "os"
-  "fmt"
+  "image/draw"
 )
 
 func main() {
   m := martini.Classic()
 
   // http://localhost:3000/hello/myname
-  m.Get("/v1/imaginize/:phrase", func(params martini.Params, res http.ResponseWriter, req *http.Request) (int, image.Image) {
+  m.Get("/v1/imaginize/:phrase", func(params martini.Params, res http.ResponseWriter, req *http.Request) (int, string) {
     words := strings.Split(params["phrase"], " ")
 
     var channels []chan image.Image
@@ -35,12 +34,14 @@ func main() {
     //   #we are getting images
     // }
 
-    image := <- channels[0]
+    left_image := <- channels[0]
+    right_image := <- channels[1]
 
-    out, _ := os.Create("test.png")
-    png.Encode(out, image)
+    combined_image := combineImages(left_image, right_image)
 
-    return 200, image 
+    out := new(bytes.Buffer)
+    png.Encode(out, combined_image)
+    return 200, string(out.Bytes())
   })
   
   m.Run()
@@ -62,8 +63,8 @@ func getUrls(word string) []string {
 }
 
 type ImageRequest struct {
-    Urls []string
-    Size uint8
+  Urls    []string    `json:"urls"`
+  Height  uint        `json:"height_px"`
 }
 
 func downloadImage(urls []string) image.Image{  
@@ -71,11 +72,10 @@ func downloadImage(urls []string) image.Image{
 
   request := ImageRequest{
     Urls: urls,
-    Size: 150,
+    Height: 150,
   }
 
   jsonStr, _ := json.Marshal(request)
-  fmt.Println(string(jsonStr))
 
   req, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
   
@@ -86,3 +86,30 @@ func downloadImage(urls []string) image.Image{
   img, _, _:= image.Decode(resp.Body)
   return img
 }
+
+func combineImages(left_image image.Image, right_image image.Image) image.Image {
+  
+  left_image_width := getWidth(left_image)
+  right_image_width := getWidth(right_image)
+  total_width := left_image_width + right_image_width
+
+  canvas := image.NewRGBA(image.Rect(0, 0, total_width, 150))
+
+  drawImageAtPosition(canvas, left_image, image.Point{0,0})
+  drawImageAtPosition(canvas, right_image, image.Point{left_image_width,0})
+  
+  return canvas
+}
+
+func getWidth(image image.Image) int{
+  return image.Bounds().Max.X
+}
+
+func drawImageAtPosition(canvas draw.Image, image_to_draw image.Image, position image.Point) {
+  sr := image.Rect(0, 0, getWidth(image_to_draw), 150)
+  r := image.Rectangle{position, position.Add(sr.Size())}
+  
+  draw.Draw(canvas, r, image_to_draw, sr.Min, draw.Src)
+  return
+}
+
